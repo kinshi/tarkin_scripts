4422492c4a1e17d905556de42e4a8fb72b18441b
swgemu <swgemu@swgemu.swgemu.com>
2013-10-23 16:47:41 -0600

    [merge] from SWGEmu 'unstable' 23OCT13

--- scripts/managers/jedi/village/old_man.lua
+++ scripts/managers/jedi/village/old_man.lua
@@ -6,17 +6,30 @@ OLD_MAN_MAX_SPAWN_DISTANCE = 64
 OLD_MAN_TEMPLATE = "old_man"
 OLD_MAN_RESPAWN_TIME = 0
 OLD_MAN_HEADING = 0
+OLD_MAN_ID_STRING = ":old_man_id"
 
 OldMan = ScreenPlay:new {}
 
 -- Spawn the old man near the player.
 -- @pCreatureObject pointer to the creature object of the player.
 function OldMan.spawnOldMan(pCreatureObject)
-	OldMan.withSceneObject(pCreatureObject, function(sceneObject)
+	return OldMan.withSceneObject(pCreatureObject, function(sceneObject)
 		local spawnPoint = getSpawnPoint(pCreatureObject, sceneObject:getWorldPositionX(), sceneObject:getWorldPositionY(), OLD_MAN_MIN_SPAWN_DISTANCE, OLD_MAN_MAX_SPAWN_DISTANCE)
 		if spawnPoint ~= nil then
-			spawnMobile(sceneObject:getZoneName(), OLD_MAN_TEMPLATE, OLD_MAN_RESPAWN_TIME, spawnPoint[1], spawnPoint[2], spawnPoint[3], OLD_MAN_HEADING, sceneObject:getParentID())
+			return spawnMobile(sceneObject:getZoneName(), OLD_MAN_TEMPLATE, OLD_MAN_RESPAWN_TIME, spawnPoint[1], spawnPoint[2], spawnPoint[3], OLD_MAN_HEADING, sceneObject:getParentID())
 		end
+		return nil
+	end)
+end
+
+-- Save the id of the old man to only enable the old man for the player he was spawned for.
+-- @param pCreatureObject pointer to the creature object of the player who got the old man spawned.
+-- @param pOldMan pointer to the creature object of the spawned old man.
+function OldMan.saveOldManIdOnPlayer(pCreatureObject, pOldMan)
+	OldMan.withCreatureObject(pCreatureObject, function(playerCreatureObject)
+		OldMan.withCreatureObject(pOldMan, function(oldManCreatureObject)
+			writeData(playerCreatureObject:getObjectID() .. OLD_MAN_ID_STRING, oldManCreatureObject:getObjectID())
+		end)
 	end)
 end
 
@@ -26,7 +39,7 @@ end
 function OldMan.isPlayerOnline(pCreatureObject)
 	return OldMan.withCreaturePlayerObject(pCreatureObject, function(playerObject)
 		return playerObject:isOnline()
-	end)
+	end) == true
 end
 
 -- Check if the player is in a building or not.
@@ -35,7 +48,7 @@ end
 function OldMan.isPlayerInABuilding(pCreatureObject)
 	return OldMan.withSceneObject(pCreatureObject, function(sceneObject)
 		return not (sceneObject:getParentID() == NOTINABUILDING)
-	end)
+	end) == true
 end
 
 -- Check if the player is in a NPC city.
@@ -51,12 +64,33 @@ function OldMan.isPlayerInNpcCity(pCreatureObject)
 	end) == true
 end
 
+-- Check if the player is in a postion where the old man can be spawned.
+-- @param pCreatureObject pointer to the creature object of the player.
+-- @return true if the player is in a position where the old man can be spawned.
+function OldMan.canOldManBeSpawned(pCreatureObject)
+	return (OldMan.isPlayerOnline(pCreatureObject) and not OldMan.isPlayerInABuilding(pCreatureObject) and not OldMan.isPlayerInNpcCity(pCreatureObject))
+end
+
+-- Try to spawn the old man and create the needed events.
+-- @param pCreatureObject pointer to the creature object of the player who should get the old man spawned.
+-- @return true if everything were successful.
+function OldMan.tryToSpawnOldMan(pCreatureObject)
+	local pOldMan = OldMan.spawnOldMan(pCreatureObject)
+	if pOldMan ~= nil then
+		OldMan.saveOldManIdOnPlayer(pCreatureObject, pOldMan)
+		return true
+	else
+		return false
+	end
+end
+
 -- Function to handle the old man event.
 -- @param pCreatureObject pointer to the creature object who should have an event created for spawning the old man.
 function OldMan:handleSpawnOldManEvent(pCreatureObject)
-	if OldMan.isPlayerOnline(pCreatureObject) and not OldMan.isPlayerInABuilding(pCreatureObject) and 
-           not OldMan.isPlayerInNpcCity(pCreatureObject) then
-		OldMan.spawnOldMan(pCreatureObject)
+	if OldMan.canOldManBeSpawned(pCreatureObject) then
+		if not OldMan.tryToSpawnOldMan(pCreatureObject) then
+			OldMan.createSpawnOldManEvent(pCreatureObject)
+		end
 	else
 		OldMan.createSpawnOldManEvent(pCreatureObject)
 	end
@@ -66,6 +100,18 @@ end
 -- @param pCreatureObject pointer to the creature object who should have an event created for spawning the old man.
 function OldMan.createSpawnOldManEvent(pCreatureObject)
 	createEvent(20000, "OldMan", "handleSpawnOldManEvent", pCreatureObject)
+end
+
+-- Check if the old man belongs to the player or not.
+-- @param pConversingPlayer pointer to the creature object of the conversing player.
+-- @param pConversingNpc pointer to the creature object of the old man.
+-- @return true if the old man belongs to the player.
+function OldMan.oldManBelongsToThePlayer(pConversingPlayer, pConversingNpc)
+	return OldMan.withCreatureObject(pConversingPlayer, function(playerCreatureObject)
+		return OldMan.withCreatureObject(pConversingNpc, function(oldManCreatureObject)
+			return readData(playerCreatureObject:getObjectID() .. OLD_MAN_ID_STRING) == oldManCreatureObject:getObjectID()
+		end) == true
+	end) == true
 end
 
 return OldMan
