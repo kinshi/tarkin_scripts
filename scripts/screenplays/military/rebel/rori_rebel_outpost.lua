7d2d4264da1a534c13af1de8311b8ea4355dc76c
gurgi1970 <jrogersjr57@gmail.com>
2013-11-17 17:26:05 +0000

    [UPDATE] Rori Rebel Outpost

--- scripts/screenplays/military/rebel/rori_rebel_outpost.lua
+++ scripts/screenplays/military/rebel/rori_rebel_outpost.lua
@@ -39,7 +39,9 @@ function RoriRebelOutpostScreenPlay:spawnSceneObjects()
 	--Players get here via F.A.R.T travel terminal at Moenia starport (Moenia is a Rebel stronghold)
 	
 end
+
 function RoriRebelOutpostScreenPlay:spawnMobiles()
+
 	--mobs
 
 		--Patrols outside the walls
@@ -81,26 +83,32 @@ function RoriRebelOutpostScreenPlay:spawnMobiles()
 	spawnMobile("rori", "sentry_specforce_sniper", 360, 3642, 96, -6477, 110, 0)
 	spawnMobile("rori", "sentry_specforce_combat_medic", 360, 3656, 96, -6463, 80, 0)
 	spawnMobile("rori", "sentry_specforce_sniper", 360, 3684, 96, -6458, 280, 0)
+
 		--Guard Tower with rifle trainer
 	spawnMobile("rori", "sentry_rebel_major", 360, 3651, 103, -6485, 90, 0)
 	spawnMobile("rori", "sentry_specforce_bombardier", 360, 3651, 103, -6489, 90, 0)
 	spawnMobile("rori", "sentry_specforce_captain", 360, 3645, 103, -6494, 180, 0)
 	spawnMobile("rori", "sentry_specforce_combat_medic", 360, -2.0, 2, 3.5, 90, 4505604)
+
 		--Guard Tower with carbine trainer
 	spawnMobile("rori", "sentry_specforce_captain", 360, 3635, 103, -6439, 91, 0)
 	spawnMobile("rori", "sentry_specforce_bombardier", 360, 3632, 103, -6432, 0, 0)
 	spawnMobile("rori", "sentry_specforce_sniper", 360, -2.5, 2.0, 3.7, 70, 4505993)
+
 		--Guard Tower with marksman trainer
 	spawnMobile("rori", "sentry_specforce_combat_medic", 360, 3680, 103, -6491, 170, 0)
 	spawnMobile("rori", "sentry_specforce_bombardier", 360, 3677, 103, -6481, 180, 0)
 	spawnMobile("rori", "sentry_specforce", 360, 3685, 103, -6485, 50, 0)
+
 		-- Tavern
 	spawnMobile("rori", "sentry_specforce_combat_medic", 360, -8.5, 0.6, -7.3, 47, 4505636)
 	spawnMobile("rori", "sentry_specforce", 360, 2.2, 0.6, -2.2, 243, 4505636)
 	spawnMobile("rori", "sentry_specforce", 360, 2.1, 0.6, -6.5, 280, 4505636)
 	spawnMobile("rori", "sentry_rebel_major", 360, 7.8, 0.6, -4.4, 270, 4505637)
+
 		--Left Storage Shed with quest givers
 	spawnMobile("rori", "sentry_specforce", 360, 0, 0.1, -6.0, 0, 4505666)
+
 		--Right Storage Shed (Cloner)
 	spawnMobile("rori", "sentry_specforce_combat_medic", 360, 5.3, 0.1, 0.9, 270, 4505609)
 	spawnMobile("rori", "sentry_specforce_squad_leader", 360, 0, 0.1, -6.0, 0, 4505608)
@@ -118,8 +126,49 @@ function RoriRebelOutpostScreenPlay:spawnMobiles()
 	spawnMobile("rori", "trainer_rifleman", 60, 3649, 104, -6491.5, 315, 0)
 	spawnMobile("rori", "trainer_weaponsmith", 60, 3681, 96, -6420, 0, 0)
 
+	-- Gate Sentries
+
+	spawnMobile("rori", "sentry_specforce", 300, 3672.2, 96.0, -6495.7, 179, 0)
+	spawnMobile("rori", "sentry_specforce", 300, 3656.9, 96.0, -6496.6, 179, 0)
+	spawnMobile("rori", "sentry_specforce", 300, math.random(12) + 3658.7, 96.1, math.random(14) + -6494.3, 179, 0)
+	spawnMobile("rori", "sentry_specforce", 300, math.random(12) + 3658.7, 96.1, math.random(14) + -6494.3, 179, 0)
+	spawnMobile("rori", "sentry_specforce", 300, math.random(12) + 3658.7, 96.1, math.random(14) + -6494.3, 179, 0)
+	spawnMobile("rori", "sentry_specforce", 300, math.random(12) + 3658.7, 96.1, math.random(14) + -6494.3, 179, 0)
+	spawnMobile("rori", "sentry_specforce", 300, math.random(12) + 3658.7, 96.1, math.random(14) + -6494.3, 179, 0)
+
+	-- Building Entrance Sentries
+
+	spawnMobile("rori", "sentry_specforce", 300, 3678.5, 96.0, -6478.3, 0, 0)
+	spawnMobile("rori", "sentry_specforce", 300, 3683.1, 96.0, -6478.3, 1, 0)
+	spawnMobile("rori", "sentry_specforce", 300, 3694.9, 96.0, -6479.2, -2, 0)
+	spawnMobile("rori", "sentry_specforce", 300, 3690.9, 96.7, -6482.6, 0, 0)
+	spawnMobile("rori", "sentry_specforce", 300, 3684.9, 96.0, -6453.0, -88, 0)
+	spawnMobile("rori", "sentry_specforce", 300, 3655.9, 96.0, -6468.8, 90, 0)
+	spawnMobile("rori", "sentry_specforce", 300, 3655.7, 96.0, -6453.9, 90, 0)
+	spawnMobile("rori", "sentry_specforce", 300, 3655.7, 96.0, -6449.8, 90, 0)	
 	
+end
+
+function RoriRebelOutpostScreenPlay:spawnSceneObjects()
+	
+	-- Turrets
+	local pTurret1 = spawnSceneObject("rori", "object/installation/turret/turret_block_med.iff", 3664.8, 96.0, -6448.1, 0, 1, 0, 0, 0)
+	setTurretFaction(pTurret1)
+	-- This sets the turrent with a due north facing towards the base entance.
+
+	local pTurret2 = spawnSceneObject("rori", "object/installation/turret/turret_block_med.iff", 3682.1, 96.0, -6472.1, 0, -0.707107, 0, 0.707107, 0)
+	setTurretFaction(pTurret2)
+	-- This sets the turrent with a due west facing towards the storage sheds
+
+end
+
+function setTurretFaction(pTurret)
+	if pTurret ~= nil then
+		local turret = LuaTangibleObject(pTurret)
+		turret:setFaction(FACTIONREBEL)
+		turret:setPvpStatusBitmask(1)
 	end
+end
 
 -- Zephyr Travel Destinations
 
