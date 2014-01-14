1f64daea2360f619feeab9c519a78f29dd3c3c30
Wol <little_wol@hotmail.com>
2014-01-14 14:57:01 +0000

    [ADDED]Aa'kuan Cave weapon groups

---
+++ scripts/mobile/weapon/groups/aakuan_carbine_weapons.lua
@@ -0,0 +1,9 @@
+aakuan_carbine_weapons = {
+	"object/weapon/ranged/carbine/carbine_dxr6.iff"
+	"object/weapon/ranged/carbine/carbine_elite.iff"
+	"object/weapon/ranged/carbine/carbine_laser.iff"
+	"object/weapon/ranged/carbine/carbine_ee3.iff"
+	"object/weapon/ranged/carbine/carbine_e11.iff"
+}
+
+addWeapon("aakuan_carbine_weapons", aakuan_carbine_weapons)
---
+++ scripts/mobile/weapon/groups/aakuan_polearm_weapons.lua
@@ -0,0 +1,7 @@
+aakuan_polearm_weapons = {
+	"object/weapon/melee/polearm/lance_vibrolance.iff"
+	"object/weapon/melee/polearm/polearm_vibro_axe.iff"
+	"object/weapon/melee/polearm/lance_nightsister.iff"
+}
+
+addWeapon("aakuan_polearm_weapons", aakuan_polearm_weapons)
