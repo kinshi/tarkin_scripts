176e1b348eb60b7827b24a9620f750a010553aae
Kinshi <cpedgin2@gmail.com>
2014-04-03 22:40:25 -0700

    [added] elevator controls and admin term to Bespin House

--- scripts/object/building/player/bespin_house.lua
+++ scripts/object/building/player/bespin_house.lua
@@ -52,11 +52,15 @@ object_building_player_bespin_house = object_building_player_shared_bespin_house
 		{"private_med_battle_fatigue", 15}
 	},
 	childObjects = {
-
+			--{templateFile = "object/tangible/sign/player/house_address.iff", x = 4.34, z = 3.4, y = 18.40, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1},
+			{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = 2.08274, z= 88.8888, y = -5.94208, ox = 0, oy = 0.991445, oz = 0, ow = 0.130526, cellid = 2, containmentType = -1},
+			{templateFile = "object/tangible/terminal/terminal_elevator_up.iff", x = 0.680352, z = 0.631912, y = 1.11112, ow = -0.258819, ox = 0, oy = 0.965926, oz = 0, cellid = 1, containmentType = -1},
+			{templateFile = "object/tangible/terminal/terminal_elevator_down.iff", x = 0.551327, z = 88.8368, y = 1.32484, ow = -0.130526, ox = 0, oy = 0.991445, oz = 0, cellid = 1, containmentType = -1}
 	},
 	constructionMarker = "object/building/player/construction/construction_player_house_corellia_large_style_01.iff",
-	length = 5,
-	width = 7
+	
+	length = 1,
+	width = 1
 }
 
 ObjectTemplates:addTemplate(object_building_player_bespin_house, "object/building/player/bespin_house.iff")
