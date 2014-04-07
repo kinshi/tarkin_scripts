105e097921efd03f440e07fccdae0ad31bb6f9b4
Kinshi <cpedgin2@gmail.com>
2014-04-07 16:32:31 -0700

    [added] some unmerged templated from the SWGEmu merge

---
+++ scripts/mobile/talus/kathikiis_assist.lua
@@ -0,0 +1,36 @@
+kathikiis_assist = Creature:new {
+	objectName = "",
+	socialGroup = "townsperson",
+	pvpFaction = "townsperson",
+	faction = "townsperson",
+	level = 100,
+	chanceHit = 1.000000,
+	damageMin = 645,
+	damageMax = 1000,
+	baseXp = 9429,
+	baseHAM = 24000,
+	baseHAMmax = 30000,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,0,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0.000000,
+	ferocity = 0,
+	pvpBitmask = NONE,
+	creatureBitmask = PACK,
+	optionsBitmask = 136,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/selonian_female.iff"},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "kathikiis_ruwahurr_mission_target_convotemplate",
+	attacks = {}
+}
+
+CreatureTemplates:addCreatureTemplate(kathikiis_assist, "kathikiis_assist")
