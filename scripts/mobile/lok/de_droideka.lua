2a3d5dba5f8734d6f39c328de90442479a59db40
Wol <little_wol@hotmail.com>
2014-04-19 13:18:48 +0100

    [UPDATE]Droid Engineer Cave Revamp
    needs first testing. to make sure all mobs are placed correctly

---
+++ scripts/mobile/lok/de_droideka.lua
@@ -0,0 +1,49 @@
+de_droideka = Creature:new {
+	objectName = "@mob/creature_names:droideka",
+	socialGroup = "droideka",
+	pvpFaction = "",
+	faction = "",
+	level = 170,
+	chanceHit = 10,
+	damageMin = 500,
+	damageMax = 700,
+	baseXp = 15461,
+	baseHAM = 35700,
+	baseHAMmax = 41900,
+	armor = 2,
+	resists = {40,40,40,0,40,-1,40,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = AGGRESSIVE + ATTACKABLE + ENEMY,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = 128,
+	diet = HERBIVORE,
+
+	templates = {"object/mobile/droideka.iff"},
+	lootGroups = {
+			groups = {
+			{
+				{group = "droid_loot", chance = 1500000},
+				{group = "nyms_common", chance = 2000000},
+				{group = "pistols", chance = 1000000},
+				{group = "carbines", chance = 1000000},
+				{group = "tailor_components", chance = 1500000},
+				{group = "clothing_attachments", chance = 1500000},
+				{group = "armor_attachments", chance = 1500000}
+			},
+			lootChance = 2500000
+		},
+	},
+	conversationTemplate = "",
+	defaultWeapon = "object/weapon/ranged/droid/droid_droideka_ranged.iff",
+	defaultAttack = "creaturerangedattack",
+}
+
+CreatureTemplates:addCreatureTemplate(de_droideka, "de_droideka")
