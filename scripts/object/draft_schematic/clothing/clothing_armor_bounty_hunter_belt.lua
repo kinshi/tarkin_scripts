f8f39f4995ef7e32ba347f89545558beedb63e25
Kinshi <cpedgin2@gmail.com>
2014-05-14 17:31:40 -0700

    [added] IHA Junk Dealer request for Kadaara [changed] start of making BH armor craftable (this is not ready for Tarkin yet)

--- scripts/object/draft_schematic/clothing/clothing_armor_bounty_hunter_belt.lua
+++ scripts/object/draft_schematic/clothing/clothing_armor_bounty_hunter_belt.lua
@@ -43,6 +43,39 @@
 
 object_draft_schematic_clothing_clothing_armor_bounty_hunter_belt = object_draft_schematic_clothing_shared_clothing_armor_bounty_hunter_belt:new {
 
+templateType = DRAFTSCHEMATIC,
+
+   customObjectName = "Bounty Hunter Armor Belt",
+
+   craftingToolTab = 2, -- (See DraftSchemticImplementation.h)
+   complexity = 40, 
+   size = 4, 
+
+   xpType = "crafting_clothing_armor", 
+   xp = 80, 
+
+   assemblySkill = "armor_assembly", 
+   experimentingSkill = "armor_experimentation", 
+   customizationSkill = "armor_customization", 
+
+   customizationOptions = {},
+   customizationStringNames = {},
+   customizationDefaults = {},
+
+   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
+   ingredientTitleNames = {"auxilary_coverage", "body", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
+   ingredientSlotType = {0, 0, 0, 0, 0, 0, 1, 1, 1},
+   resourceTypes = {"hide_leathery_lok", "hide_scaley", "fiberplast_corellia", "metal", "petrochem_inert_polymer", "hide_wooly", "object/tangible/component/armor/shared_armor_segment_composite.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceQuantities = {10, 10, 5, 5, 3, 2, 1, 1, 1},
+   contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
+
+
+   targetTemplate = "object/tangible/wearables/armor/bounty_hunter/armor_bounty_hunter_belt.iff",
+
+   additionalTemplates = {
+             }
+
+
 }
 
 ObjectTemplates:addTemplate(object_draft_schematic_clothing_clothing_armor_bounty_hunter_belt, "object/draft_schematic/clothing/clothing_armor_bounty_hunter_belt.iff")
