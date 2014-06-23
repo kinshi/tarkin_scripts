e1f24d5e50730ede89fb12a358df31f7c93620e3
Wol <little_wol@hotmail.com>
2014-06-23 19:12:39 +0100

    [Removed]Temporarily removed res deeds

--- scripts/loot/items.lua
+++ scripts/loot/items.lua
@@ -1031,7 +1031,7 @@ includeFile("items/tarkin_custom/goggles_s05.lua")
 
 --Loot
 
-includeFile("items/tarkin_custom/resource_deed.lua")
+--includeFile("items/tarkin_custom/resource_deed.lua")
 includeFile("items/tarkin_custom/av_21_schematic.lua")
 includeFile("items/tarkin_custom/av_21_powerplant.lua")
 
