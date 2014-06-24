df4e62b190ff4ec8f65b821863705adc2aef96f4
Kinshi <cpedgin2@gmail.com>
2014-06-23 23:49:51 -0700

    [fixed] Wolor is a muppet for messing up the .gitignore file in the /conf folder

--- scripts/object/tangible/wearables/armor/bounty_hunter/armor_bounty_hunter_helmet.lua
+++ scripts/object/tangible/wearables/armor/bounty_hunter/armor_bounty_hunter_helmet.lua
@@ -109,7 +109,7 @@ object_tangible_wearables_armor_bounty_hunter_armor_bounty_hunter_helmet = objec
 	experimentalGroupTitles = {"null", "null", "null", "exp_durability", "exp_quality", "exp_resistance", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
 	experimentalSubGroupTitles = {"null", "null", "sockets", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
 	experimentalMin = {0, 0, 0, 1000, 1, 30000, 25, 22, 300, 1, 0, 5, 0},
-	experimentalMax = {0, 0, 0, 5000, 40, 50000, 15, 13, 180, 1, 0, 55, 0},
+	experimentalMax = {0, 0, 0, 5000, 40, 75000, 15, 13, 180, 1, 0, 55, 0},
 	experimentalPrecision = {0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 4, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 
