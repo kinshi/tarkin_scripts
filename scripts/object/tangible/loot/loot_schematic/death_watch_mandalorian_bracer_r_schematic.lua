72c5cf68ff11596c1f2840dffa327591e390e20a
Wol <little_wol@hotmail.com>
2014-06-28 18:57:48 +0100

    [Added]Craftable Mandalorian Armor
    pushed for first test on dev env

--- scripts/object/tangible/loot/loot_schematic/death_watch_mandalorian_bracer_r_schematic.lua
+++ scripts/object/tangible/loot/loot_schematic/death_watch_mandalorian_bracer_r_schematic.lua
@@ -42,7 +42,13 @@
 
 
 object_tangible_loot_loot_schematic_death_watch_mandalorian_bracer_r_schematic = object_tangible_loot_loot_schematic_shared_death_watch_mandalorian_bracer_r_schematic:new {
-
+	templateType = LOOTSCHEMATIC,
+	customName = "Mandalorian Armor Right Bracer Schematic",
+	objectMenuComponent = {"cpp", "LootSchematicMenuComponent"},
+	attributeListComponent = "LootSchematicAttributeListComponent",
+	requiredSkill = "crafting_armorsmith_master",
+	targetDraftSchematic = "object/draft_schematic/clothing/clothing_armor_mandalorian_bracer_r.iff",
+	targetUseCount = 1
 }
 
 ObjectTemplates:addTemplate(object_tangible_loot_loot_schematic_death_watch_mandalorian_bracer_r_schematic, "object/tangible/loot/loot_schematic/death_watch_mandalorian_bracer_r_schematic.iff")
--- scripts/object/tangible/loot/loot_schematic/death_watch_mandalorian_chest_plate_schematic.lua
+++ scripts/object/tangible/loot/loot_schematic/death_watch_mandalorian_chest_plate_schematic.lua
@@ -42,7 +42,13 @@
 
 
 object_tangible_loot_loot_schematic_death_watch_mandalorian_chest_plate_schematic = object_tangible_loot_loot_schematic_shared_death_watch_mandalorian_chest_plate_schematic:new {
-
+	templateType = LOOTSCHEMATIC,
+	customName = "Mandalorian Armor Chest Plate Schematic",
+	objectMenuComponent = {"cpp", "LootSchematicMenuComponent"},
+	attributeListComponent = "LootSchematicAttributeListComponent",
+	requiredSkill = "crafting_armorsmith_master",
+	targetDraftSchematic = "object/draft_schematic/clothing/clothing_armor_mandalorian_chest_plate.iff",
+	targetUseCount = 1
 }
 
 ObjectTemplates:addTemplate(object_tangible_loot_loot_schematic_death_watch_mandalorian_chest_plate_schematic, "object/tangible/loot/loot_schematic/death_watch_mandalorian_chest_plate_schematic.iff")
