75a7e8d63ee19117e81e2cd83a97d6da7fb26dd3
Kinshi <cpedgin2@gmail.com>
2014-07-04 17:06:46 -0700

    [fixed] restored lair files for peko pekos of all types

--- scripts/mobile/lair/creature_dynamic/naboo_peko_peko_flight_neutral_none.lua
+++ scripts/mobile/lair/creature_dynamic/naboo_peko_peko_flight_neutral_none.lua
@@ -1,12 +1,11 @@
 naboo_peko_peko_flight_neutral_none = Lair:new {
-	mobiles = {{"peko_peko",1}},
+	mobiles = {},
 	spawnLimit = 15,
 	buildingsVeryEasy = {},
 	buildingsEasy = {},
 	buildingsMedium = {},
 	buildingsHard = {},
 	buildingsVeryHard = {},
-	buildingType = "none",
 }
 
 addLairTemplate("naboo_peko_peko_flight_neutral_none", naboo_peko_peko_flight_neutral_none)
