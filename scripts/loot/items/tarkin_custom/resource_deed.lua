6dcd036296c9a6044e30b822a382ce5d212bdaf6
Kinshi <cpedgin2@gmail.com>
2014-07-07 18:53:18 -0700

    [replaced] Resource deed drop with resource deed voucher drop, reenabled resource deed loot group

--- scripts/loot/items/tarkin_custom/resource_deed.lua
+++ scripts/loot/items/tarkin_custom/resource_deed.lua
@@ -3,15 +3,12 @@
 resource_deed = {
 	minimumLevel = 0,
 	maximumLevel = -1,
-	customObjectName = "",
-	directObjectTemplate = "object/tangible/veteran_reward/resource.iff",
+	customObjectName = "Redeemable for one(1) Crate of Free Resources",
+	directObjectTemplate = "object/tangible/loot/misc/ticket_expired.iff",
 	craftingValues = {
 	},
 	customizationStringNames = {},
 	customizationValues = {},
-	skillMods = {
-
-	}
 }
 
 addLootItemTemplate("resource_deed", resource_deed)
