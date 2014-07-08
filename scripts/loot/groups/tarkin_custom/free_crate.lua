77a53ea736bd957c6bfab0361a6dda51a6255e7a
Kinshi <cpedgin2@gmail.com>
2014-07-07 19:25:34 -0700

    [changed] renamed resource deed group - possible name conflict

---
+++ scripts/loot/groups/tarkin_custom/free_crate.lua
@@ -0,0 +1,10 @@
+resource_deed = {
+	description = "",
+	minimumLevel = 0,
+	maximumLevel = 0,
+	lootItems = {
+		{itemTemplate = "resource_deed", weight = 10000000}
+	}
+}
+
+addLootGroupTemplate("resource_deed", resource_deed)
---
+++ scripts/loot/items/tarkin_custom/free_crate.lua
@@ -0,0 +1,12 @@
+resource_deed = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Redeemable for one(1) Crate of Free Resources",
+	directObjectTemplate = "object/tangible/loot/misc/ticket_expired.iff",
+	craftingValues = {
+	},
+	customizationStringNames = {},
+	customizationValues = {},
+}
+
+addLootItemTemplate("resource_deed", resource_deed)
