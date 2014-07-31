08c44033ec08bca073c85dfca03ac73c3da8ee63
Freddiec <Socome@users.noreply.github.com>
2014-07-30 23:53:52 -0500

    Final
    eq

---
+++ scripts/loot/groups/tarkin_custom/tarkin_house_award.lua
@@ -0,0 +1,10 @@
+tarkin_house_award = {
+	desription = "",
+	minimumLevel = 0,
+	maximumLevel = 0,	
+	lootItems = {
+		{itemTemplate = "tarkin_award", weight = 10000000},	
+	}
+}
+
+addLootGroupTemplate("tarkin_house_award", tarkin_house_award)
