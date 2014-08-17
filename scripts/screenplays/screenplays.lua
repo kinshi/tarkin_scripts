52bc9e942a7df02520093d7db8363970a74d36a3
stacy19201325 <stacy_19201325@hotmail.com>
2014-08-17 15:05:14 +0000

    [Fixed] The rest of Raavi's files

--- scripts/screenplays/screenplays.lua
+++ scripts/screenplays/screenplays.lua
@@ -280,7 +280,7 @@ includeFile("treasure_map/TreasureMapMenuComponent.lua")
 --Tests
 --includeFile("tests/options_bitmask_test.lua")
 --includeFile("event/stresstest_20120128.lua")
-
+includeFile("tests/anniversary_2year.lua")
 
 --Race Tracks
 includeFile("racetracks/racetracks.lua")
