08002e7477ceef58393a70660ed8eeda347d9bb2
Kinshi <cpedgin2@gmail.com>
2014-08-25 09:44:38 -0700

    [fixed] Lake Retreat shuttle object spawn

--- scripts/screenplays/poi/naboo_amidalas_beach.lua
+++ scripts/screenplays/poi/naboo_amidalas_beach.lua
@@ -9,9 +9,17 @@ registerScreenPlay("NabooAmidalasBeachScreenPlay", true)
 
 function NabooAmidalasBeachScreenPlay:start()
 	if (isZoneEnabled("naboo")) then
+		self:spawnSceneObjects()
 		self:spawnMobiles()
 
 	end
+end
+
+function NabooAmidalasBeachScreenPlay:spawnSceneObjects()
+
+--Lake Retreat SHuttle
+spawnSceneObject("naboo", "object/creature/npc/theme_park/player_shuttle.iff", -5474.0, 1-149.0, -21.0, 0, 180)
+
 end
 
 function NabooAmidalasBeachScreenPlay:spawnMobiles()
