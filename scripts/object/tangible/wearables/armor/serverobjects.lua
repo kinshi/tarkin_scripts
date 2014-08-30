544ee4225214eb74b299156bec2d633b66f36955
Kinshi <cpedgin2@gmail.com>
2014-08-30 15:54:15 -0700

    [fixed] missing serverobjects entry for rebel battle armor

--- scripts/object/tangible/wearables/armor/serverobjects.lua
+++ scripts/object/tangible/wearables/armor/serverobjects.lua
@@ -67,10 +67,11 @@ includeFile("tangible/wearables/armor/zam/serverobjects.lua")
 
 --tarkin CU armor
 
-includeFile("tangible/wearables/armor/assault_trooper/serverobjects.lua")
-includeFile("tangible/wearables/armor/clone_trooper/serverobjects.lua")
-includeFile("tangible/wearables/armor/rebel_assault/serverobjects.lua")
 includeFile("tangible/wearables/armor/scout_trooper/serverobjects.lua")
+includeFile("tangible/wearables/armor/assault_trooper/serverobjects.lua")
+includeFile("tangible/wearables/armor/rebel_assault/serverobjects.lua")
+includeFile("tangible/wearables/armor/rebel_battle/serverobjects.lua")
+includeFile("tangible/wearables/armor/clone_trooper/serverobjects.lua")
 
 
 
