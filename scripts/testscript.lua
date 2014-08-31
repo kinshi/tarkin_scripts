b4248dc93f8c3e2ac541c808a244f0ede25801e2
Kinshi <cpedgin2@gmail.com>
2014-08-30 19:18:12 -0700

    [merge] swgemu unstable branch 30AUG14  (stability fixes)

--- scripts/testscript.lua
+++ scripts/testscript.lua
@@ -1,10 +1,10 @@
-function printf(...) io.write(string.format(unpack(arg))) end
+function printf(...) io.write(string.format(unpack({...}))) end
 
 local creature = LuaCreatureObject(nil)
 
 function runScript(creatureObject)
 	creature:_setObject(creatureObject)
   	
-  	--creature:setHAM(0, math.random(99))
+  	--creature:setHAM(0, getRandomNumber(99))
   	credits = creature:getBankCredits()  
 end
