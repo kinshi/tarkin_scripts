2334433567a0237bd9095067bf955117c7c784b1
stacy19201325 <stacy_19201325@hotmail.com>
2014-09-07 13:53:05 +0000

    [Updated] Raavi Or'zeth (an artist) to normal version

--- scripts/screenplays/screenplays.lua
+++ scripts/screenplays/screenplays.lua
@@ -283,7 +283,7 @@ includeFile("treasure_map/TreasureMapMenuComponent.lua")
 --Tests
 --includeFile("tests/options_bitmask_test.lua")
 --includeFile("event/stresstest_20120128.lua")
-includeFile("tests/anniversary_2year.lua")
+includeFile("tests/painting_vendor.lua")
 
 --Race Tracks
 includeFile("racetracks/racetracks.lua")
