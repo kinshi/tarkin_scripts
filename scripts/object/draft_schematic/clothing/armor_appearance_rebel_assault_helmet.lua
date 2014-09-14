b961690fc0bae74108a9df1958b2c99282c5d42d
Wol <little_wol@hotmail.com>
2014-09-14 11:30:24 +0100

    [added] remaining CU armour stuff for initial test

--- scripts/object/draft_schematic/clothing/armor_appearance_rebel_assault_helmet.lua
+++ scripts/object/draft_schematic/clothing/armor_appearance_rebel_assault_helmet.lua
@@ -65,8 +65,8 @@ object_draft_schematic_armor_armor_appearance_rebel_assault_helmet = object_draf
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"auxilary_coverage", "body", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
    ingredientSlotType = {0, 0, 0, 0, 0, 0, 1, 1, 1},
-   resourceTypes = {"ore_intrusive", "fuel_petrochem_solid_known", "fiberplast_naboo", "aluminum", "copper_beyrllius", "hide_wooly", "object/tangible/component/armor/shared_armor_segment_composite.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
-   resourceQuantities = {70, 70, 35, 40, 30, 30, 3, 1, 1},
+   resourceTypes = {"hide_leathery_lok", "hide_scaley", "fiberplast_corellia", "metal", "petrochem_inert_polymer", "hide_wooly", "object/tangible/component/armor/shared_armor_segment_composite.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceQuantities = {40, 40, 20, 25, 25, 30, 1, 2, 1},
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
