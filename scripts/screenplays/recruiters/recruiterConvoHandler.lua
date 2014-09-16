443c6327bf74e6e6a8996a2f4e7e7f2ade41ee87
Kinshi <cpedgin2@gmail.com>
2014-09-15 17:24:44 -0700

    [changed] Recruiter handler to set new faction member to onleave, not SF

--- scripts/screenplays/recruiters/recruiterConvoHandler.lua
+++ scripts/screenplays/recruiters/recruiterConvoHandler.lua
@@ -49,7 +49,7 @@ function RecruiterConvoHandler:runScreenHandlers(conversationTemplate, conversin
 
 		elseif (screenID == "accept_join") then
 			player:setFaction(recruiterScreenplay:getRecruiterFactionHashCode(conversingNPC))
-			playerObject:setFactionStatus(3)
+			playerObject:setFactionStatus(1)
 
 		elseif (screenID == "accepted_go_overt") then
 			playerObject:setFactionStatus(3)
