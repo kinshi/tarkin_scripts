a5b359c5c8e7cb2080598bcf3aea813866d264b1
Kinshi <cpedgin2@gmail.com>
2014-12-21 14:34:40 -0800

    {merge] untracked files missed in previous merge

---
+++ scripts/mobile/tatooine/wild_bladeback_boar.lua
@@ -0,0 +1,40 @@
+wild_bladeback_boar = Creature:new {
+	objectName = "@mob/creature_names:quest_hero_of_tatooine_ferocious_beast",
+	socialGroup = "boar",
+	pvpFaction = "",
+	faction = "",
+	level = 39,
+	chanceHit = 0.44,
+	damageMin = 355,
+	damageMax = 420,
+	baseXp = 3915,
+	baseHAM = 9100,
+	baseHAMmax = 11100,
+	armor = 0,
+	resists = {30,30,30,30,30,30,30,-1,-1},
+	meatType = "meat_herbivore",
+	meatAmount = 120,
+	hideType = "hide_leathery",
+	hideAmount = 80,
+	boneType = "bone_mammal",
+	boneAmount = 50,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK + KILLER,
+	optionsBitmask = 0,
+	diet = CARNIVORE,
+
+	templates = {"object/mobile/zucca_boar_hue.iff"},
+	scale = 1.25,
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "",
+	attacks = {
+		{"knockdownattack","knockdownChance=50"},
+		{"stunattack","stunChance=50"}
+	}
+}
+
+CreatureTemplates:addCreatureTemplate(wild_bladeback_boar, "wild_bladeback_boar")
