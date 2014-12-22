fe88718606b459aeef98ab8a24c3fe59978513b8
Kinshi <cpedgin2@gmail.com>
2014-12-21 22:17:16 -0800

    [fixed] even more lua comma/brace BS

--- scripts/object/building/player/jedi_house.lua
+++ scripts/object/building/player/jedi_house.lua
@@ -46,8 +46,8 @@ object_building_player_jedi_house = object_building_player_shared_jedi_house:new
 	baseMaintenanceRate = 10,
 	allowedZones = {"corellia", "dantooine", "lok", "naboo", "rori", "talus", "tatooine", "endor", "yavin4", "dathomir"},
 	constructionMarker = "object/building/player/construction/construction_player_house_corellia_large_style_01.iff",
-	length = 5,
-	width = 7
+	length = 1,
+	width = 1,
 	publicStructure = 0,
 	skillMods = {
 		{"private_medical_rating", 100},
@@ -57,6 +57,7 @@ object_building_player_jedi_house = object_building_player_shared_jedi_house:new
 	childObjects = {
 			--{templateFile = "object/tangible/sign/player/house_address.iff", x = 4.34, z = 3.4, y = 18.40, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1},
 			{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = -4.98199, z = 1.0082, y = 2.42309, ow = -0.707107, ox = 0, oz = 0, oy = 0.707107, cellid = 1, containmentType = -1},
-	},
+	}
+}
 
 ObjectTemplates:addTemplate(object_building_player_jedi_house, "object/building/player/jedi_house.iff")
--- scripts/object/building/player/sith_house.lua
+++ scripts/object/building/player/sith_house.lua
@@ -45,6 +45,9 @@ object_building_player_sith_house = object_building_player_shared_sith_house:new
 	lotSize = 2,
 	baseMaintenanceRate = 10,
 	allowedZones = {"corellia", "dantooine", "lok", "naboo", "rori", "talus", "tatooine", "endor", "yavin4", "dathomir"},
+	constructionMarker = "object/building/player/construction/construction_player_house_corellia_large_style_01.iff",
+	length = 1,
+	width = 1,
 	publicStructure = 0,
 	skillMods = {
 		{"private_medical_rating", 100},
@@ -52,11 +55,11 @@ object_building_player_sith_house = object_building_player_shared_sith_house:new
 		{"private_med_battle_fatigue", 15}
 	},
 	childObjects = {
-
-	},
-	constructionMarker = "object/building/player/construction/construction_player_house_corellia_large_style_01.iff",
-	length = 5,
-	width = 7
+--			{templateFile = "object/tangible/sign/player/house_address.iff", x = 4.34, z = 3.4, y = 18.40, ox = 0, oy = 0.707107, oz = 0, ow = 0.707107, cellid = -1, containmentType = -1},
+			{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = 4.5965, z = 0.999999, y = -1.97029, ow = -0.707107, ox = 0, oz = 0, oy = 0.707107, cellid = 1, containmentType = -1},
+--			{templateFile = "object/tangible/terminal/terminal_elevator_up.iff", x = -2.66109, z = -0.513074, y = 0.0544102, ow = -0.707107, ox = 0, oz = 0, oy = 0.707107, cellid = 7, containmentType = -1},
+--			{templateFile = "object/tangible/terminal/terminal_elevator_down.iff", x = -2.65908, z = 5.39344, y = -0.0155522, ow = -0.707107, ox = 0, oz = 0, oy = 0.707107, cellid = 7, containmentType = -1}
+	}
 }
 
 ObjectTemplates:addTemplate(object_building_player_sith_house, "object/building/player/sith_house.iff")
