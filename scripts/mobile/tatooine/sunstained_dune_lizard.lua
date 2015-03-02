d5f2a5ae1b85e81786a1b8942a515c88a7488bc5
James Rogers <jrogersjr57@comcast.net>
2015-03-01 22:48:46 -0500

    [UPDATE] Removed dizzy attack from the MOB

--- scripts/mobile/tatooine/sunstained_dune_lizard.lua
+++ scripts/mobile/tatooine/sunstained_dune_lizard.lua
@@ -33,7 +33,8 @@ sunstained_dune_lizard = Creature:new {
 	weapons = {"creature_spit_small_yellow"},
 	conversationTemplate = "",
 	attacks = {
-		{"dizzyattack","dizzyChance=50"}
+		{"blindattack","blindChance=50"},
+		{"stunattack","stunChance=50"}
 	}
 }
 
