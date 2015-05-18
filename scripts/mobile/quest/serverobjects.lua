9ab4dd07770ffbbcadda32ca7015180168fed611
Kinshi <cpedgin2@gmail.com>
2015-05-17 19:44:48 -0700

    [disabled] hero of tat quests until full questline and rewards are complete

--- scripts/mobile/quest/serverobjects.lua
+++ scripts/mobile/quest/serverobjects.lua
@@ -8,14 +8,14 @@ includeFile("quest/theater_manager.lua")
 includeFile("quest/painting_artist.lua")
 
 --Hero of Tatooine
-includeFile("quest/tatooine/hero_of_tat/hero_of_tat_bh.lua")
-includeFile("quest/tatooine/hero_of_tat/hero_of_tat_farmer.lua")
-includeFile("quest/tatooine/hero_of_tat/hero_of_tat_farmers_child.lua")
-includeFile("quest/tatooine/hero_of_tat/hero_of_tat_farmers_wife.lua")
-includeFile("quest/tatooine/hero_of_tat/hero_of_tat_hermit.lua")
-includeFile("quest/tatooine/hero_of_tat/hero_of_tat_smuggler.lua")
-includeFile("quest/tatooine/hero_of_tat/hero_of_tat_smuggler_captain.lua")
-includeFile("quest/tatooine/hero_of_tat/wild_bladeback_boar.lua")
+--includeFile("quest/tatooine/hero_of_tat/hero_of_tat_bh.lua")
+--includeFile("quest/tatooine/hero_of_tat/hero_of_tat_farmer.lua")
+--includeFile("quest/tatooine/hero_of_tat/hero_of_tat_farmers_child.lua")
+--includeFile("quest/tatooine/hero_of_tat/hero_of_tat_farmers_wife.lua")
+--includeFile("quest/tatooine/hero_of_tat/hero_of_tat_hermit.lua")
+--includeFile("quest/tatooine/hero_of_tat/hero_of_tat_smuggler.lua")
+--includeFile("quest/tatooine/hero_of_tat/hero_of_tat_smuggler_captain.lua")
+--includeFile("quest/tatooine/hero_of_tat/wild_bladeback_boar.lua")
 
 --Generic
 includeFile("quest/generic/businessman_target_thief_award.lua")
